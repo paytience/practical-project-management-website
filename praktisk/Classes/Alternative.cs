021b001f835940f95b8e18e4c3ba72a6a76f3518
simen <simentha@stud.ntnu.no>
2019-10-22 15:52:57 +0200

    Added more intutive way of adding questions

--- praktisk/Classes/Alternative.cs
+++ praktisk/Classes/Alternative.cs
@@ -7,16 +7,18 @@ namespace praktisk
 {
     public class Alternative
     {
-        public Alternative(string alternativeId, string alternativeQuestionId, string correct, string alternativeText)
+        public Alternative(string alternativeId, string alternativeQuestionId, string correct, string alternativeText, string alternativeQuizId)
         {
             this.AlternativeId = alternativeId;
             this.AlternativeQuestionId = alternativeQuestionId;
             this.Correct = correct;
             this.AlternativeText = alternativeText;
+            this.AlternativeQuizId = alternativeQuizId;
         }
         public string AlternativeId { set; get; }
         public string AlternativeQuestionId { set; get; }
         public string Correct { set; get; }
         public string AlternativeText { set; get; }
+        public string AlternativeQuizId { get; set; }
     }
 }
